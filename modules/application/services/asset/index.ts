8922e06e6a8667f274f4aace63566c4738275d3f
jeffreywallphd-ai <jeffreywallphd@gmail.com>
2026-05-07 16:27:48 +0000

    Add built-in asset definition catalog

--- modules/application/services/asset/index.ts
+++ modules/application/services/asset/index.ts
@@ -5,3 +5,4 @@ export * from "./validate-asset-binding.service";
 export * from "./validate-asset-composition.service";
 export * from "./asset-resource-backed-mapping.service";
 export * from "./built-in-asset-definition-seeding.service";
+export * from "./built-ins";
